84ba34436598581612eb356e266f6b401d6e3b2f
jacob <jacob.180524@nv.ccsd.net>
2019-11-21 10:53:15 -0800

    jj

---
+++ src/org/display/Handler.java
@@ -0,0 +1,14 @@
+package org.display;
+
+import org.game.Game;
+import org.world.World;
+
+public class Handler {
+
+	private Game game;
+	private World world;
+	
+	public Handler(Game game) {
+		
+	}
+}
